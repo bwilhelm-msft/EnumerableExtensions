84521758abe3ed2edc9cdfb0f120551eb6ec7d54
Bryan Wilhelm <bryan.wilhelm@live.com>
2022-01-22 10:21:28 -0500

    Remove generic implementations

--- EnumerableExtensions/Extensions.cs
+++ EnumerableExtensions/Extensions.cs
@@ -1,7 +1,6 @@
 ﻿namespace EnumerableExtensions;
 
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
+using System.Collections;
 
 public static class Extensions
 {
@@ -11,20 +10,22 @@ public static class Extensions
     /// <typeparam name="T">The item type of the enumerable</typeparam>
     /// <param name="enumerable">The enumerable</param>
     /// <returns><c>true</c> if the enumerable is null or empty; otherwise <c>false</c></returns>
-    public static bool IsNullOrEmpty<T>(this IEnumerable<T>? enumerable)
+    public static bool IsNullOrEmpty(this IEnumerable? enumerable)
     {
         return enumerable is null || !enumerable.GetEnumerator().MoveNext();
     }
 
     /// <summary>
-    /// Determines whether the first enumerable instance has the same length as the second enumerable instance. The two instances do not need to have the same item type. An instance that is null or empty is considering to have a length of 0.
+    /// Determines whether the first enumerable instance has the same length as the second enumerable instance.
+    /// The two instances do not need to have the same item type. An instance that is null or empty is 
+    /// considered to have a length of 0.
     /// </summary>
     /// <typeparam name="TFirst">The type of the first instance</typeparam>
     /// <typeparam name="TSecond">The type of the second instance</typeparam>
     /// <param name="first">The first enumerable instance</param>
     /// <param name="second">The second enumerable instance</param>
     /// <returns><c>true</c> if the two instance have the same length; otherwise <c>false</c>.</returns>
-    public static bool HasSameLengthAs<TFirst, TSecond>(this IEnumerable<TFirst>? first, IEnumerable<TSecond>? second)
+    public static bool HasSameLengthAs(this IEnumerable? first, IEnumerable? second)
     {
         if (first is null)
         {
